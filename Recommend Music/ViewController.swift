ec2dac1521ff8b8a7b1996ad788edba218349c5d
verianggoro <veryanggoro007@gmail.com>
2021-07-05 22:26:20 +0700

    init

--- Recommend Music/ViewController.swift
+++ Recommend Music/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputUserId: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        inputUserId.setLeftImage(imageName: "ic_search")
     }
 
 
 }
-
+extension UITextField{
+    func setLeftImage(imageName:String) {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        imageView.image = UIImage(named: imageName)
+        self.leftView = imageView;
+        self.leftViewMode = .always
+    }
+}
